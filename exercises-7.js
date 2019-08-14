62f90327c1e45d6dcf6fac8144a5e4264043b851
straf16 <depoksugutamu621@gmail.com>
2019-08-15 00:08:07 +0700

    mengerjakan exercises-7

---
+++ exercises-7.js
@@ -0,0 +1,14 @@
+function urutkanAbjad(str) {
+    // you can only write your code here!
+    
+    var x = str.split('');
+    var result = x.sort();
+    return result.join('');
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
